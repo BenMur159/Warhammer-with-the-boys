00b52980c6a3e7318836c66f4cae7692d261c07e
Benedikt Murer <benediktmurer@gmail.com>
2025-09-08 18:24:13 +0200

    added vanishing-appearing mainHeader

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
 console.log('we are alive');
+mainPageInit();
+
+function mainPageInit() {
+  renderHeader();
+}
+
+function renderHeader() {
+  const headerContainer = document.createElement('div');
+  headerContainer.classList.add('js-header-container');
+  console.log(headerContainer.classList);
+
+  const headerContainerHTML = `
+    <div class="main-header-background-image">
+      <h1 class="webside-main-header js-main-header">Warhammer with the Boys</h2>
+    </div>
+    <div style="height: 2000px;"></div>
+  `;
+
+  headerContainer.innerHTML = headerContainerHTML;
+  document.body.append(headerContainer);
+}
+
+window.addEventListener('scroll', () => {
+  const mainHeader = document.querySelector(`.js-main-header`);
+  if (window.scrollY >= 200) {
+    mainHeader.classList.add('vanishing-header');
+  } else {
+    mainHeader.classList.remove('vanishing-header');
+  }
+});
